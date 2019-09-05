6306db5d30c1ac5f6cad4af144d3b753aa4c2a40
Mohammad Ashraf <g.mohammad.ashraf.99@gmail.com>
2019-09-05 03:01:59 +0200

    add server/user/client, enhancing tracker

--- tracker.py
+++ tracker.py
@@ -1,17 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
-import time
 import smtplib
 
-def check_price(URL, header, initial_price, title):
+def get_price(URL, header):
     page = requests.get(URL, headers=header)
 
     soup1 = BeautifulSoup(page.content, "html.parser")
     soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
-    new_price = soup2.find(id="priceblock_ourprice").get_text()
-    new_price = float(new_price[1:])
-    if new_price < initial_price:
-        send_mail(URL, title, initial_price, new_price, user_email)
+    title = (soup2.find(id="productTitle").get_text()).strip()
+    price = soup2.find(id="priceblock_ourprice").get_text()
+    price = float(price[1:])
+    return price
+
+def get_title(URL, header):
+    page = requests.get(URL, headers=header)
+
+    soup1 = BeautifulSoup(page.content, "html.parser")
+    soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+    title = (soup2.find(id="productTitle").get_text()).strip()
+    return title
 
 def send_mail(URL, title, initial_price, new_price, user_email):
     server = smtplib.SMTP('smtp.gmail.com', 587)    # Establishing a connection between client and the gmail server
@@ -31,20 +38,3 @@ def send_mail(URL, title, initial_price, new_price, user_email):
     )
     server.quit()
     print("Email has been sent!")
-
-
-URL = input("Enter the product's URL: ")
-user_email = input('Enter your E-mail to be notified: ')
-header = {
-    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
-
-page = requests.get(URL, headers=header)
-
-soup1 = BeautifulSoup(page.content, "html.parser")
-soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
-title = (soup2.find(id="productTitle").get_text()).strip()
-initial_price = soup2.find(id="priceblock_ourprice").get_text()
-initial_price = float(initial_price[1: ])
-
-while(True):
-    check_price(URL, header, initial_price, title)
