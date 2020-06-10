cc67b37aa0bf41c91f460c90ced7b9d571310561
Mohammad Ashraf <g.mohammad.ashraf.99@gmail.com>
2020-06-10 14:04:11 +0200

    added the db schema (only the entries table)

--- app/models.py
+++ app/models.py
@@ -1,11 +1,14 @@
 from app.init_app import db
 
-class User(db.Model):
 
-    def __init__(self):
-        id = db.Column(db.Integer, primary_key=True)
-        uEmail = db.Column(db.String(120), index=True, unique=True)
-        pURL = db.Column(db.String(128))
+class Entry(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_email = db.Column(db.String(120), index=True, nullable=False)
+    product_url = db.Column(db.String(128), index=True, nullable=False)
 
     def __repr__(self):     # This method defines how objects should be printed
-        return '<User {}>'.format(self.username)
+        return '<Entry: user_email: {email} | product_url: {url}>'.format(email=self.user_email, url=self.product_url)
+
+
+#class post(db.Model):
