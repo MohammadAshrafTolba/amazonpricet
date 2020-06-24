d645df9ccc5314e50571ecb247b620083c0d5fa0
Mohammad Ashraf <g.mohammad.ashraf.99@gmail.com>
2020-06-24 05:21:57 +0200

    updating the config file to include the email verifier API secret key

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ basedir = basedir.replace('\\', '/')
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'some-secret-string-pattern'
-
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + basedir + '/app.db'
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    EMAIL_VERIFIER_KEY = 'at_BZhozlaBYNPyyLQDuEO6TdG47f85Q'
