115c8f50b636be34acb2123cbfbed0402f19c480
Mohammad Ashraf <g.mohammad.ashraf.99@gmail.com>
2020-06-24 05:18:20 +0200

    renewing the index page style, adding loading page, updating the routing file

--- app/init_app.py
+++ app/init_app.py
@@ -2,11 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_email_verifier import EmailVerifier
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+email_verifier = EmailVerifier(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,41 @@
 from app.init_app import app
 from flask import render_template, request
+from app.handlers.request_handler import RequestHandler
 
-@app.route('/')
-@app.route('/index')
-def my_form():
-    return render_template('index.html')
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    uEmail = request.form['uEmail']
-    pURL = request.form['pURL']
-    print(uEmail, pURL)
-    return uEmail, pURL
+error_message_1 = error_message_2 = confirmation_message = ''
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+@app.route('/index.html', methods=['GET', 'POST'])
+def home_page():
+
+    # globalize error/confirmation messages
+    global error_message_1, error_message_2, confirmation_message
+
+    if request.method == 'GET':
+        return render_template('index.html', confirmation_message=confirmation_message, error_message_1=error_message_1, error_message_2=error_message_2)
+
+    # if request.method == POST
+
+    # initializing error/confirmation messages for each request as they are global variables
+    error_message_1 = error_message_2 = confirmation_message = ''
+
+    form_data = request.form['form_data']
+    handler = RequestHandler(form_data)
+    request_valid, status = handler.handle_request()
+    if request_valid == True:
+        confirmation_message = status
+        return render_template('index.html')
+    else:
+        error_messages = status.split('\n')
+        error_message_1 = error_messages[0]
+        if len(error_messages) == 2:
+            error_message_2 = error_messages[1]
+        return render_template('index.html')
+
+
+@app.route('/loading_page.html', methods=['POST'])
+def loading_page():
+    form_data = request.form['uEmail'] + ' ' + request.form['pURL']
+    return render_template('loading_page.html', form_data=form_data)
